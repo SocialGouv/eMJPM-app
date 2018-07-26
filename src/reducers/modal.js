c47c81649bdac67b827b037f159aff60f4305ec7
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-07-26 15:39:28 +0200

    Update async redux

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -1,5 +1,3 @@
-import apiFetch from "../components/communComponents/Api";
-
 const modal = (
   state = {
     openModal: false,
@@ -9,28 +7,15 @@ const modal = (
   },
   action
 ) => {
-  console.log("modal reducer", state, action);
   switch (action.type) {
     case "OPEN_MODAL":
       return {
         openModal: true,
-        currentMandataire: action.currentMandataire
+        currentMandataire: action.currentMandataire,
+        currentEtablissementsForSelectedMandataire:
+          action.currentEtablissementsForSelectedMandataire,
+        allTisForOneMandataire: action.allTisForOneMandataire
       };
-    // return apiFetch(`/mandataires/${action.currentMandataire.id}/tisEtablissement`).then(
-    //   currentEtablissementsForSelectedMandataire =>
-    //     apiFetch(`/mandataires/${action.currentMandataire.id}/tis-by-mandataire`)
-    //       .then(allTisForOneMandataire => {
-    //         return {
-    //           currentEtablissementsForSelectedMandataire,
-    //           allTisForOneMandataire,
-    //           openModal: true,
-    //           currentMandataire: action.currentMandataire
-    //         };
-    //       })
-    //       .catch(e => {
-    //         console.log(e);
-    //       })
-    // );
     case "CLOSE_MODAL":
       return {
         openModal: false
