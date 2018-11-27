323ef363019f791f2226cc1333bf9276c875f0f0
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-11-27 10:46:55 +0100

    update poly

--- pages/_document.js
+++ pages/_document.js
@@ -29,8 +29,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="description" content="" />
           <meta name="author" content="" />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.0.0/polyfill.js" />
-          <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
+          <script src="https://cdn.polyfill.io/v2/polyfill.js?features=es6">
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="stylesheet" href="/static/css/react-table.css" />
           <link rel="stylesheet" href="/static/css/leaflet/leaflet.css" />
